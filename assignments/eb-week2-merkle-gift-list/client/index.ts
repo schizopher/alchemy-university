7eca52f150d273d5e17f44a693a2d67378cb42dc
Wilson Lau <hello@wilsonplau.com>
2023-03-22 14:52:02 +0900

    feat: finish assignment

--- assignments/eb-week2-merkle-gift-list/client/index.ts
+++ assignments/eb-week2-merkle-gift-list/client/index.ts
@@ -6,11 +6,15 @@ const serverUrl = "http://localhost:1225";
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list?
-
+  const merkleTree = new MerkleTree(niceList);
+  const index = Math.floor(Math.random() * niceList.length);
+  const name = niceList[index];
+  console.log({ name });
+  const proof = merkleTree.getProof(index);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name,
+    proof,
   });
-
   console.log({ gift });
 }
 
