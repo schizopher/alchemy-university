0110d6ecf6556cc18c0eb5e784998462601cdaab
Wilson Lau <hello@wilsonplau.com>
2023-03-25 17:19:21 +0900

    feat: finish eb-week6-mint-nft

--- assignments/eb-week6-mint-nft/scripts/deploy.js
+++ assignments/eb-week6-mint-nft/scripts/deploy.js
@@ -7,11 +7,14 @@ async function main() {
   await nft.deployed();
 
   console.log("NFT deployed to:", nft.address);
- 
+
   // mint one to yourself!
   const signer0 = await ethers.provider.getSigner(0);
   // update the IPFS CID to be your metadata CID
-  await nft.safeMint(await signer0.getAddress(), "ipfs://QmbCwoJB7g9b86BfEahsjzyzzNx3WEu7Jc1opXWgeTXFgb");
+  await nft.safeMint(
+    await signer0.getAddress(),
+    "ipfs://QmSvhp484htExJq7SCmqabXCYhqwv4UADtmcvD9851vng2"
+  );
 
   console.log("NFT Minted!");
 }
